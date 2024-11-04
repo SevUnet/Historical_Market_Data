b4634759f0ccac7f4a32563e38c33bc1ae24e1b4
SevUnet <51000504+SevUnet@users.noreply.github.com>
2024-11-04 16:53:49 +0100

    V0.0.1

---
+++ HMData/HMData/Auth.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace WindowsFormsApp6
+{
+    public partial class Auth : Form
+    {
+        public static string username;
+        public static string nickname;
+        public Auth()
+        {
+            InitializeComponent();
+
+            //DB
+            
+            label3.Text = "Server : " + DatabaseManager.con.State.ToString();
+            label3.ForeColor = Color.Green;
+            Console.WriteLine("Connection SQL Server established !", Console.ForegroundColor = ConsoleColor.Green);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+
+            string pass = null;
+            string access = null;
+
+
+            try
+            {
+                MySqlCommand commandsql = new MySqlCommand("SELECT * FROM accounts WHERE Login = '" + login.Text + "'", DatabaseManager.con);
+                MySqlDataReader auth = commandsql.ExecuteReader();
+
+                while (auth.Read())
+                {
+                    pass = auth["Password"].ToString();
+                    access = auth["Access"].ToString();
+                    nickname = auth["Nickname"].ToString();
+
+                    if (pass == password.Text & access == "1")
+                    {
+                        username = login.Text;
+                        DataRequest.accountsparam();
+                        //MessageBox.Show(DataRequest.nickname);
+                        //Auth.ActiveForm.Close();
+                        Console.WriteLine("User : " + nickname + " logged", Console.ForegroundColor = ConsoleColor.White);
+                        //Auth.ActiveForm.Hide();
+                        //Main f = new Main();
+                        //f.Show();
+                        //Auth.ActiveForm.Hide();
+                    }
+                    else if (access != "1")
+                    {
+                        MessageBox.Show("Account hasn't been activated, please contact the administrator");
+                    }
+                    else if (pass != password.Text)
+                    {
+                        MessageBox.Show("Password wrong");
+                    }
+                    else if(pass == "" || pass == null)
+                    {
+                        MessageBox.Show("Login wrong");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Login or password wrong");
+                    }
+                }
+            }
+            catch (Exception ter)
+            {
+                MessageBox.Show("Exception: " + ter.Message);
+            }
+        }
+    }
+}
---
+++ HMData/HMData/Database_query.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace HMData
+{
+    class Database_query
+    {
+        public static string host;
+        public static string database;
+        public static string user;
+        public static string password;
+        public static MySqlConnection con;
+
+        public static void ConnectionDB()
+        {
+            host = "localhost";
+            database = "loto";
+            user = "root";
+            password = "";
+
+            string constr = "Server=" + host + ";DATABASE=" + database + ";User ID=" + user + ";Password=" + password + ";Pooling=true;Charset=utf8;SslMode=none;";
+
+            try
+            {       
+                con = new MySqlConnection(constr);
+                con.Open();
+            }
+            catch (IOException Ex)
+            {
+                MessageBox.Show("Connection failed     " + Ex.ToString());
+            }
+        }
+
+    }
+}
